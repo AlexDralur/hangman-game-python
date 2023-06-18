896bde3064cbe84704b86138f24a430d0185993e
Alexander Alves <aalexanderalves@gmail.com>
2023-06-18 20:28:36 +0000

    Fix error in hangman function

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def hangman_design():
     Structure for the design of the hangman.
     Based on the amount of errors of the user, the design change.
     """
-    if error_counter = 0:
+    if error_counter == 0:
         print('    ______    ')
         print('    |    |    ')
         print('         |    ')
@@ -14,7 +14,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
     
-    elif error_counter = 1:
+    elif error_counter == 1:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -23,7 +23,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
     
-    elif error_counter = 2:
+    elif error_counter == 2:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -32,7 +32,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
         
-    elif error_counter = 3:
+    elif error_counter == 3:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -41,7 +41,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
 
-    elif error_counter = 4:
+    elif error_counter == 4:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -50,7 +50,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
 
-    elif error_counter = 5:
+    elif error_counter == 5:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -59,7 +59,7 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
 
-    elif error_counter = 6:
+    elif error_counter == 6:
         print('    ______    ')
         print('    |    |    ')
         print('    O    |    ')
@@ -68,3 +68,15 @@ def hangman_design():
         print('         |    ')
         print('       *****  ')
 
+
+def main_screen():
+    print("""
+           ________            __  __                                      
+ /_  __/ /_  ___     / / / /___ _____  ____ _____ ___  ____ _____ 
+  / / / __ \/ _ \   / /_/ / __ `/ __ \/ __ `/ __ `__ \/ __ `/ __ \
+ / / / / / /  __/  / __  / /_/ / / / / /_/ / / / / / / /_/ / / / /
+/_/ /_/ /_/\___/  /_/ /_/\__,_/_/ /_/\__, /_/ /_/ /_/\__,_/_/ /_/ 
+                                    /____/                        
+          """)
+
+main_screen()
