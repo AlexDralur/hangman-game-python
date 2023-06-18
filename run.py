7218e6928142de21c0e98467f9a8a39471910ead
Alexander Alves <aalexanderalves@gmail.com>
2023-06-18 20:09:51 +0000

    Add function for the hangman design

--- run.py
+++ run.py
@@ -1,3 +1,67 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def hangman_design():
+
+    if error_counter = 0:
+        print('    ______    ')
+        print('    |    |    ')
+        print('         |    ')
+        print('         |    ')
+        print('         |    ')
+        print('         |    ')
+        print('       *****  ')
+    
+    elif error_counter = 1:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('         |    ')
+        print('         |    ')
+        print('         |    ')
+        print('       *****  ')
+    
+    elif error_counter = 2:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('    |    |    ')
+        print('         |    ')
+        print('         |    ')
+        print('       *****  ')
+        
+    elif error_counter = 3:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('   /|    |    ')
+        print('         |    ')
+        print('         |    ')
+        print('       *****  ')
+
+    elif error_counter = 4:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('   /|\   |    ')
+        print('         |    ')
+        print('         |    ')
+        print('       *****  ')
+
+    elif error_counter = 5:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('   /|\   |    ')
+        print('   /     |    ')
+        print('         |    ')
+        print('       *****  ')
+
+    elif error_counter = 3:
+        print('    ______    ')
+        print('    |    |    ')
+        print('    O    |    ')
+        print('   /|\   |    ')
+        print('   / \   |    ')
+        print('         |    ')
+        print('       *****  ')
+
